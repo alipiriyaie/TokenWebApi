ba2c7fed6862c19d683e5ea26cc00d2b232f1dad
Ali Piriyaie <ali.piriyaie@schraml.de>
2023-06-21 11:02:36 +0200

    changed launchSettings to not start the browser when debug mode is started and changed endpoint to access weatherdata

--- TokenWebApi/Controllers/WeatherForecastController.cs
+++ TokenWebApi/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace TokenWebApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
+        [HttpGet("data")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
